ff1dfa9d2ddf5083ca870bec2c9d5e9de40792e0
Ronit Kataria <77673121+ronit450@users.noreply.github.com>
2023-10-24 00:10:39 +0500

    vectorization completed

--- src/poetry_tokenizer.py
+++ src/poetry_tokenizer.py
@@ -10,16 +10,20 @@ class Token:
 
     def df_Maker(self, data):
         df =  pd.read_csv(data)
-        print(df.head())
         return df
     
     def Tf_IDF(self):
         tfidf_vectorizer = TfidfVectorizer()
         tfidf_matrix = tfidf_vectorizer.fit_transform(self.data)
+        dense_tfidf_matrix = tfidf_matrix.toarray()
+        return dense_tfidf_matrix
     
     def word_embeddings(self):
-        poetry_sentences = [couplet.split() for couplet in self.data['couplets']]
+        
+        poetry_sentences = [couplet.split() for couplet in self.data['Couplets']]
         model = Word2Vec(poetry_sentences, vector_size=100, window=5, min_count=1, sg=0)
-        word_embeddings = [model.wv[word] for word in poetry_sentences.split() if word in model.wv]
+        word_embedding = [model.wv[word] for word in poetry_sentences.split() if word in model.wv]
+        return word_embedding
 
 a = Token('data/dataCompiled.csv')
+print(a.Tf_IDF())
